6de3443d57eb626f6d5d5f5b477a47a60bd3b212
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2022-04-22 23:26:19 -0300

    Exercicio sobre Sobrecarga

--- ExercitandoJavaComDio/src/main/java/pacote/exercitandojavacomdio/ExercitandoJavaComDio.java
+++ ExercitandoJavaComDio/src/main/java/pacote/exercitandojavacomdio/ExercitandoJavaComDio.java
@@ -6,7 +6,7 @@ public class ExercitandoJavaComDio {
 
     public static void main(String[] args) {
         
-        
+        /*
         Exer1 exer1 = new Exer1();
         exer1.escolhaNumeros();
         exer1.contas();
@@ -22,8 +22,12 @@ public class ExercitandoJavaComDio {
         Exer3.calcular(1000, exer3.getDuasParcelas());
         Exer3.calcular(1000, exer3.getTresParcelas());
         Exer3.calcular(1000, 5);
+        */
         
-        
-        
+        System.out.println("Exercicio Quadrilatero");
+        Quadrilatero_Sobrecarga.area(3);
+        Quadrilatero_Sobrecarga.area(5d, 5d);
+        Quadrilatero_Sobrecarga.area(7, 8, 9);
+        Quadrilatero_Sobrecarga.area(5f, 5f);
     }
 }
